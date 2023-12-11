17e045cf9cc33010a02dce61492d26f6ea820d1c
Sky Lou <skyloutyr@gmail.com>
2023-12-11 11:01:19 +0300

    Updated the version number.

--- VTT/Program.cs
+++ VTT/Program.cs
@@ -24,7 +24,7 @@
                 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
                 Code = Assembly.GetExecutingAssembly();
                 Console.Clear();
-                Version = new Version(1, 2, 9);
+                Version = new Version(1, 2, 10);
                 ArgsManager.Parse(args);
                 if (ArgsManager.TryGetValue("server", out int port))
                 {

afee50038147ae9da92a285549995134fe850897
Sky Lou <skyloutyr@gmail.com>
2025-05-12 14:23:08 +0300

    Optimized WeightedList to use binary search to find items with desired weight faster

--- VTT/Util/WeightedRandom.cs
+++ VTT/Util/WeightedRandom.cs
@@ -4,6 +4,8 @@
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Security.Cryptography;
 
     public class WeightedRandom
     {
@@ -70,22 +72,32 @@
             get => this._list[index]; 
             set
             {
-                this._totalWeight -= this._list[index].Weight;
-                this._totalWeight += this._list[index].Weight;
+                WeightedItem<T> now = this._list[index];
+                int wDelta = now.Weight - value.Weight;
+                if (wDelta != 0)
+                {
+                    this.UpdateRightElements(index + 1, wDelta);
+                }
+
+                this._totalWeight += wDelta;
                 this._list[index] = value;
             }
         }
 
         public void Add(WeightedItem<T> item)
         {
+            item.localCumulativeWeight = this._totalWeight + item.Weight;
             this._list.Add(item);
             this._totalWeight += item.Weight;
         }
 
         public bool Remove(WeightedItem<T> item)
         {
-            if (this._list.Remove(item))
+            int i = this._list.IndexOf(item);
+            if (i != -1)
             {
+                this.UpdateRightElements(i + 1, -item.Weight);
+                this._list.Remove(item);
                 this._totalWeight -= item.Weight;
                 return true;
             }
@@ -97,30 +109,74 @@
         {
             rand ??= WeightedRandom.rand;
             int weight = rand.Next(this.TotalWeight) + 1;
-            for (int i = 0; i < this.Count; ++i)
+            if (this.Count < 16) // Arbitrary, for small collections a quick check may be faster
             {
-                if (weight <= this[i].Weight)
+                for (int i = 0; i < this.Count; ++i)
                 {
-                    return this[i];
+                    WeightedItem<T> itm = this._list[i];
+                    if (weight <= itm.localCumulativeWeight)
+                    {
+                        return itm;
+                    }
                 }
 
-                weight -= this[i].Weight;
+                return this[^1];
             }
+            else // Use binary search
+            {
+                int low = 0;
+                int high = this._list.Count - 1;
+                while (low <= high)
+                {
+                    int mid = low + ((high - low) / 2);
+                    WeightedItem<T> itm = this._list[mid];
+                    int wEnd = itm.localCumulativeWeight;
+                    if (weight > wEnd)
+                    {
+                        low = mid + 1;
+                        continue;
+                    }
 
-            return this[^1];
+                    if (weight < wEnd - itm.Weight)
+                    {
+                        high = mid - 1;
+                        continue;
+                    }
+
+                    return itm;
+                }
+
+                return this[^1];
+            }
         }
 
         public int IndexOf(WeightedItem<T> item) => this._list.IndexOf(item);
         public void Insert(int index, WeightedItem<T> item)
         {
+            if (index < this.Count - 1)
+            {
+                WeightedItem<T> itmHere = this[index];
+                item.localCumulativeWeight = itmHere.localCumulativeWeight - itmHere.Weight + item.Weight;
+                this.UpdateRightElements(index + 1, item.Weight);
+            }
+            else
+            {
+                item.localCumulativeWeight = this._totalWeight + item.Weight;
+            }
+
             this._list.Insert(index, item);
             this._totalWeight += item.Weight;
         }
 
         public void RemoveAt(int index)
         {
-            this._totalWeight -= this._list[index].Weight;
-            this._list.RemoveAt(index);
+            if (index >= 0 && index < this.Count)
+            {
+                WeightedItem<T> itm = this._list[index];
+                this.UpdateRightElements(index, -itm.Weight);
+                this._list.RemoveAt(index);
+                this._totalWeight -= itm.Weight;
+            }
         }
 
         public void Clear()
@@ -133,17 +189,33 @@
         public void CopyTo(WeightedItem<T>[] array, int arrayIndex) => this._list.CopyTo(array, arrayIndex);
         public IEnumerator<WeightedItem<T>> GetEnumerator() => this._list.GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private void UpdateRightElements(int at, int added)
+        {
+            if (at >= this.Count - 1)
+            {
+                return;
+            }
+
+            Span<WeightedItem<T>> items = CollectionsMarshal.AsSpan(this._list);
+            for (int i = at + 1; i < this.Count; ++i)
+            {
+                items[i].localCumulativeWeight += added;
+            }
+        }
     }
 
-    public readonly struct WeightedItem<T>
+    public struct WeightedItem<T>
     {
         public T Item { get; }
         public int Weight { get; }
+        internal int localCumulativeWeight;
 
         public WeightedItem(T t, int weight)
         {
             this.Item = t;
             this.Weight = weight;
+            this.localCumulativeWeight = weight;
         }
     }
 }

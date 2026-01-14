cddb53e5b77426684879bb928a72a5063e39a7cf
Sky Lou <skyloutyr@gmail.com>
2026-01-14 17:48:01 +0300

    Updated the version number.

--- VTT/Program.cs
+++ VTT/Program.cs
@@ -56,7 +56,7 @@
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
             Code = Assembly.GetExecutingAssembly();
             Console.Clear();
-            Version = new Version(1, 2, 32);
+            Version = new Version(1, 2, 33);
             ArgsManager.Parse(args);
             IOVTT.LoadLocations();
             if (ArgsManager.TryGetValue(LaunchArgumentKey.HeadlessServerPort, out int port))
